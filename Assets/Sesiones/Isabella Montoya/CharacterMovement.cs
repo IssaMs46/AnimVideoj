79bc9660638e03f5e4db9a2e634925b79399198f
Chava <isams2004.1@gmail.com>
2025-02-11 07:38:58 -0500

    arreglado todo

--- Assets/Sesiones/Isabella Montoya/CharacterMovement.cs
+++ Assets/Sesiones/Isabella Montoya/CharacterMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Animator))] 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField] private Camera camera;
     [SerializeField] private FloatDampener speedX;
     [SerializeField] private FloatDampener speedY;
 
@@ -19,6 +20,15 @@ public class CharacterMovement : MonoBehaviour
         Vector2 inputValue  = ctx.ReadValue<Vector2>();
         speedX.TargetValue = inputValue.x;
         speedY.TargetValue = inputValue.y;
+
+        Vector3 floorNormal=transform.up;
+        Vector3 cameraRealForward = camera.transform.forward;
+        float angleInterpolator = Mathf.Abs(Vector3.Dot(cameraRealForward, floorNormal));
+        Vector3 cameraForward= Vector3.Lerp(cameraRealForward, camera.transform.up, angleInterpolator).normalized;
+        Vector3 characterForward = Vector3.ProjectOnPlane(cameraForward, floorNormal);
+
+        Debug.DrawLine(transform.position, transform.position + characterForward * 2, Color.magenta, 5);
+
 
     }
 
