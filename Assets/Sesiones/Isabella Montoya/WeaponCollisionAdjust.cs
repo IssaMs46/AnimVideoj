313708c01927dac5667e37a6ff134810936a4174
Chava <isams2004.1@gmail.com>
2025-03-20 11:44:42 -0500

    hasta aca es lo de ik con apuntado

--- Assets/Sesiones/Isabella Montoya/WeaponCollisionAdjust.cs
+++ Assets/Sesiones/Isabella Montoya/WeaponCollisionAdjust.cs
@@ -11,9 +11,11 @@ public class WeaponCollisionAdjust : MonoBehaviour
         public RaycastHit hitInfo;
     }
 
-    [SerializeField] private Transform handIk;
+    [SerializeField] private AvatarIKGoal triggerHand = AvatarIKGoal.RightHand;
+    [SerializeField] private AvatarIKGoal holdingHand = AvatarIKGoal.LeftHand;
 
     [SerializeField] private Transform weaponReference;
+    [SerializeField] private Transform weaponHandle;
     [SerializeField] private float weaponLenght;
     [SerializeField] private float profileThickness;
 
@@ -21,7 +23,9 @@ public class WeaponCollisionAdjust : MonoBehaviour
 
     private Animator anim;
     RayResult rayResult;
-    private float offset;
+    [SerializeField] private FloatDampener offset;
+
+    private Character character;
 
     private void SolveOffset()
     {
@@ -30,13 +34,14 @@ public class WeaponCollisionAdjust : MonoBehaviour
         result.result = Physics.SphereCast(result.ray, profileThickness, out result.hitInfo, weaponLenght, layerMask);
         rayResult = result;
 
-        offset  = Mathf.Max(0, weaponLenght  -  Vector3.Distance(rayResult.hitInfo.point , weaponReference.position)) * -1;
+        offset.TargetValue  = Mathf.Max(0, weaponLenght  -  Vector3.Distance(rayResult.hitInfo.point , weaponReference.position)) * -1;
 
     }
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        character = GetComponent<Character>();
     }
 
     private void FixedUpdate()
@@ -46,16 +51,33 @@ public class WeaponCollisionAdjust : MonoBehaviour
 
     private void OnAnimatorIK(int layerIndex)
     {
-        Vector3 originalIkPosition = anim.GetIKPosition(AvatarIKGoal.RightHand);
-        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
-        anim.SetIKPosition(AvatarIKGoal.RightHand, originalIkPosition + weaponReference.forward  * offset); 
+        offset.Update();
+
+        if (character.IsAiming)
+        {
+
+            Vector3 originalIkPosition = anim.GetIKPosition(triggerHand);
+            anim.SetIKPositionWeight(triggerHand, 1);
+            anim.SetIKPosition(triggerHand, originalIkPosition + transform.forward  * offset.CurrentValue);
+
+            anim.SetIKPositionWeight(holdingHand, 1);
+            anim.SetIKPosition(holdingHand, weaponHandle.position);
+
+        }
+        else
+        {
+            anim.SetIKPositionWeight(triggerHand, 0);
+            anim.SetIKPositionWeight(holdingHand, 0);
+        }
+
+
     }
 
 
-    private void Update()
-    {
-        handIk.Translate(transform.forward * offset);
-    }
+    //private void Update()
+    //{
+    //    handIk.Translate(transform.forward * offset);
+    //}
 
 #if UNITY_EDITOR
 
