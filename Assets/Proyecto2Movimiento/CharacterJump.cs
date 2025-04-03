2675391e771401c407c19c85379f1a7658e1a32a
IssaMs46 <isams2004.1@gmail.com>
2025-04-02 23:00:09 -0500

    ya se mueve y salta

--- Assets/Proyecto2Movimiento/CharacterJump.cs
+++ Assets/Proyecto2Movimiento/CharacterJump.cs
@@ -4,39 +4,30 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody), typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))] 
 public class CharacterJump : MonoBehaviour, ICharacterComponent
 {
-    private Rigidbody rb;
     private Animator anim;
-    [SerializeField] private float jumpForce = 5f;
     private bool isGrounded;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        // Verifica si está en el suelo usando un Raycast
         isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
-
-        if (isGrounded)
-        {
-            anim.ResetTrigger("Jump"); // Resetea el trigger para que vuelva a Locomotion
-            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-        }
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
     {
         if (ctx.started && isGrounded)
         {
-            Debug.Log("Jumping!");
-            anim.SetTrigger("Jump"); // Usamos SetTrigger en vez de SetBool
-            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log("Jump animation triggered!");
+            anim.SetTrigger("Jump"); // Activa la animación de salto
         }
     }
 
